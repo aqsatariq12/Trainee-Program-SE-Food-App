ffec43542b514add50f55802df54cff5638db98c
aqsatariq12 <aqsatariq2452003@gmail.com>
2026-07-01 14:32:10 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/customer/Home";
+import MenuItemDetail from "./pages/customer/MenuItemDetail";
+import Offers from "./pages/customer/Offers";
+import RestaurantDetail from "./pages/customer/RestaurantDetail";
+import OrderTracking from "./pages/customer/OrderTracking";
+import Login from "./pages/customer/Login";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Routes>
+        <Route element={<Navbar/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<MenuItemDetail />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/restaurants" element={<RestaurantDetail />} />
+          <Route path="/track-order" element={<OrderTracking />} />
+
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
